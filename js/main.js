b38cbeb8b1ceecd838bd168cd007c41a4cd3dec5
Shanthanu Bhardwaj <xanth@google.com>
2019-04-10 17:36:05 -0700

    Add Typescript source

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function greet(person) {
+    return "Hello, " + person + "!";
+}
+var user = "World";
+document.body.innerHTML = greet(user);
---
+++ ts/main.ts
@@ -0,0 +1,8 @@
+function greet(person) {
+    return "Hello, " + person + "!";
+}
+
+let user = "World";
+
+document.body.innerHTML = greet(user);
+
