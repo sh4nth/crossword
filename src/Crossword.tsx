332feacc405f98122f7d8ae779b09b8c6401d3b9
Shanthanu Bhardwaj <xanth@google.com>
2019-04-12 13:24:19 -0700

    Make the squares clickable to black out or not

--- src/Crossword.tsx
+++ src/Crossword.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Square, boxSize } from "./Square";
+import { Square, boxSize, BoxState } from "./Square";
 
 type State = {
-    boxes: Array<Square>;
+    boxes: Array<Array<BoxState>>;
 };
 
 function shouldBeBlack(i:number, j:number) {
@@ -13,33 +13,38 @@ function shouldBeBlack(i:number, j:number) {
 const N = 5;
 
 export class Crossword extends Component<{}, State> {
+
+
+    // public onKeyPress(key: KeyboardEvent) {
+    //     console.log("keyEvent");
+    //     this.setState(state => {
+    //         const list = ;
+    //         state.boxes[0][0].letter = "A";
+    //         return state.boxes;
+    //     });
+    // }
+
     constructor(props: State) {
         super(props);
         let boxes = [];
         for (var i = 0; i < N; i++) {
+            let row = []
             for (var j = 0; j < N; j++) {
-                boxes.push(new Square({id: "B" + i + "-" + j, fillable: shouldBeBlack(i,j), letter: "A", x: i, y: j, clueNumber:"" + (1+i+N*j) }));
+                row.push({id: i + "-" + j, fillable: shouldBeBlack(i,j), letter: "", x: i, y: j, clueNumber:"" + (1+i+N*j) });
             }
+            boxes.push(row);
         }
         this.state = { boxes: boxes };
-    }
-
-    renderBoxes = () => {
-        let renderedBoxes: Array<any> = [];
-        this.state.boxes.forEach(box => {
-            renderedBoxes.push(box.render());
-        });
-        return renderedBoxes;
-    };
-
-    public componentDidMount() {
-        console.log("ComponentDidMount Crossword");
     }
 
     render() {
         return (<div className="Crossword">
             <svg id="crossword-svg" viewBox={"0 0 " + N*boxSize + " " + N*boxSize} xmlns="http://www.w3.org/2000/svg">
-                {this.renderBoxes()}
+                {this.state.boxes.map((row) => (
+                    row.map((b) =>
+                    <Square key={b.id} fillable={b.fillable} y={b.y} x={b.x} id={b.id} letter={b.letter} clueNumber={b.clueNumber} />
+                    )
+                ))}
             </svg>
         </div>);
     }
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,7 +1,7 @@
-import React, { Component, CSSProperties, SVGAttributes } from 'react';
+import React, { Component, CSSProperties, SVGAttributes, KeyboardEvent } from 'react';
 
 
-export type State = {
+export type BoxState = {
     x: number
     y: number
     id: string
@@ -41,37 +41,55 @@ const textStyle = {
 const groupStyle = {
     pointerEvents: 'bounding-box'
 };
-export class Square extends Component<{}, State> {
-    constructor(props: State) {
+export class Square extends Component<BoxState, BoxState> {
+    constructor(props: BoxState) {
         super(props);
-        this.state = props;
-    }
-
-    public componentDidMount() {
-        console.log("ComponentDidMount" + this.state.id);
+        this.state = {
+            x: props.x,
+            y: props.y,
+            id: props.id,
+            letter: props.letter,
+            clueNumber: props.clueNumber,
+            fillable: props.fillable,
+        };
     }
 
     public onClick() {
-        console.log("clicked " + this.state.id);
-        let oldFillable = this.state.fillable;
-        if (this.componentDidMount) {
         this.setState((state) => {
-            return {fillable: !state.fillable};
+            return { fillable: !state.fillable };
         });
-        } else {
-            console.log("DID NOT MOUNT + " + this.state.id);
-        }
-        console.log("Now ... " + this.state.id + " tried " + !oldFillable + ", and is " + this.state.fillable);
     }
 
     public render() {
-        console.log("Rendering ... " + this.state.id + " is " + this.state.fillable);
         let svgElements = [];
-        svgElements.push(<rect key={"box" + this.state.id} onClick={(e) => this.onClick()} x={this.state.x * boxSize} y={this.state.y * boxSize} width={boxSize} height={boxSize} style={getStyle(this.state.fillable)}></rect>);
+        svgElements.push(
+            <rect
+            key={"box" + this.state.id}
+                onClick={(e) => this.onClick()}
+                x={this.state.x * boxSize}
+                y={this.state.y * boxSize}
+                width={boxSize}
+                height={boxSize}
+                style={getStyle(this.state.fillable)}>
+            </rect>);
         if (this.state.fillable) {
-            svgElements.push(<text key={"letter" + this.state.id} onClick={(e) => this.onClick()} x={(this.state.x + 0.35) * boxSize} y={(this.state.y + 0.65) * boxSize} style={textStyle}>{this.state.letter}</text>);
+            svgElements.push(
+                <text
+                    key={"letter" + this.state.id}
+                    onClick={(e) => this.onClick()}
+                    x={(this.state.x + 0.35) * boxSize}
+                    y={(this.state.y + 0.65) * boxSize}
+                    style={textStyle}>{this.state.letter}
+                </text>);
             if (this.state.clueNumber != "") {
-                svgElements.push(<text key={"clue" + this.state.id} onClick={(e) => this.onClick()} x={this.state.x * boxSize + 2} y={this.state.y * boxSize + 10} style={clueStyle}>{this.state.clueNumber}</text>);
+                svgElements.push(
+                    <text
+                        key={"clue" + this.state.id}
+                        onClick={(e) => this.onClick()}
+                        x={this.state.x * boxSize + 2}
+                        y={this.state.y * boxSize + 10}
+                        style={clueStyle}>{this.state.clueNumber}
+                    </text>);
             }
         }
         return (<g key={this.state.id}>{svgElements}</g>);
