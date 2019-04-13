0ba8433f27ce2d88e973b8475d8912bd710dcbbf
Shanthanu Bhardwaj <xanth@google.com>
2019-04-12 17:12:00 -0700

    Updates

---
+++ precache-manifest.4e679bc83c85afbc2a4412b56805ef32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e06261f0ae2e001eb0d2",
+    "url": "/crossword/static/js/runtime~main.59db1c93.js"
+  },
+  {
+    "revision": "9f0e0d44ebccd9da0ee3",
+    "url": "/crossword/static/js/main.02791c92.chunk.js"
+  },
+  {
+    "revision": "db74183d028adc6c50bf",
+    "url": "/crossword/static/js/2.27835c0f.chunk.js"
+  },
+  {
+    "revision": "9f0e0d44ebccd9da0ee3",
+    "url": "/crossword/static/css/main.9d539552.chunk.css"
+  },
+  {
+    "revision": "2726bc8aa47c7b0c50769bb90e8e2ad6",
+    "url": "/crossword/index.html"
+  }
+];
